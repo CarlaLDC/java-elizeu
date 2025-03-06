91530b2989818af4d1015b9f809f4a7b65d222cd
Carla Lima <158164730+CarlaLDC@users.noreply.github.com>
2025-03-06 17:03:44 -0300

    L02B.java

--- L02B.java
+++ L02B.java
@@ -10,19 +10,28 @@ a. Ler dois valores numéricos inteiros e apresentar o resultado da diferença d
 */
 
 public class L02B {
-    public static void main(String[] args) {
+
+    public static void main(String[]args){
       Scanner ler = new Scanner(System.in);
 
-      System.out.println("Informe um numero");
-      int numeroUm = ler.nextInt();
+      System.out.println("informe o primeiro numero");
+      int numero1 = ler.nextInt();
 
-        if ( numeroUm < 0 ){
-            int multiplicacao = numeroUm * -1;
-            System.out.println(multiplicacao);
+      System.out.println("informe o segundo numero");
+      int numero2 = ler.nextInt();
+
+        if (numero1 > numero2){
+            int diferenca1 = numero1 - numero2;
+            System.out.println("A diferença entre os números é: " + diferenca1);
+        }
+
+        else if (numero2 > numero1){
+            int diferenca2 = numero2 - numero1;
+            System.out.println("A diferença entre os números é: " + diferenca2);
         }
 
         else {
-            System.out.println(numeroUm);
+            System.out.println("Os números são iguais");
         }
 
 
